e3eb3523fa40b1d5209fdc77c184152113b77544
kim-jingyu <swc05161@gmail.com>
2024-08-11 18:07:32 +0900

    init backend

---
+++ backend/src/main/java/blog/jingyu/JingyuApplication.java
@@ -0,0 +1,13 @@
+package blog.jingyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingyuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/blog/jingyu/JingyuApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.jingyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
