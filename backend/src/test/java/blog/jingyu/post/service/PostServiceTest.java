1513ba851d822b947b82cc4fbb4f92b246f885d1
kim-jingyu <swc05161@gmail.com>
2024-10-14 19:56:01 +0900

    [BACK] 테스트

--- backend/src/test/java/blog/jingyu/post/service/PostServiceTest.java
+++ backend/src/test/java/blog/jingyu/post/service/PostServiceTest.java
@@ -1,8 +1,5 @@
 package blog.jingyu.post.service;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
 //@SpringBootTest
 //@Transactional
 class PostServiceTest {
