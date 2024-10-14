b64e287eb6de5f7234c91e19a9b4e6c349e7c95b
kim-jingyu <swc05161@gmail.com>
2024-10-14 21:11:43 +0900

    [BACK] 테스트

--- backend/src/test/java/blog/jingyu/post/service/PostServiceTest.java
+++ backend/src/test/java/blog/jingyu/post/service/PostServiceTest.java
@@ -120,4 +120,5 @@ class PostServiceTest {
         assertThat(postResponse.hashtags().size()).isEqualTo(2);
         assertThat(postResponse.hashtags().get(0).content()).isEqualTo("변경된 해시태그1");
     }
+
 }
