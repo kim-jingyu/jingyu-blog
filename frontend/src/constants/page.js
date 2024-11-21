c741b54de2ea6a5237ee70928d7db9ef525b20af
kim-jingyu <swc05161@gmail.com>
2024-11-22 03:25:23 +0900

    fix: page - login path

--- frontend/src/constants/page.js
+++ frontend/src/constants/page.js
@@ -2,7 +2,7 @@ import GoogleLoginCallback from '../components/login/google/GoogleCallBack';
 import KakaoLoginCallback from '../components/login/kakao/KakaoCallBack';
 import Post from '../pages/post/Post';
 import NotFound from '../pages/error/NotFound';
-import Login from '../pages/Login/Login';
+import Login from '../pages/login/Login';
 import MainList from '../pages/main-list/MainList'
 import PostDetail from '../pages/post/PostDetail';
 
